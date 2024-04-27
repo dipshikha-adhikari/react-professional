508c4adb048e8329f864ee27bebad591b4e63107
akbiv <dipshikhaadhikari96@gmail.com>
2024-04-27 16:03:38 +0545

    Add coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,12 @@ export default {
     preset: "ts-jest",
     testEnvironment: "jsdom",
     setupFilesAfterEnv: ["./src/jest.setup.ts"],
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: -100,
+        },
+    },
 };
